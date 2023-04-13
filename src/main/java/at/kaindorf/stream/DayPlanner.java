cf331538e1b343ab6f67eb105e98e924dca00dd9
simon <simon.schoeggler@hotmail.com>
2023-04-13 09:06:31 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/stream/DayPlanner.java
@@ -0,0 +1,110 @@
+package at.kaindorf;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * <h3>Created by IntelliJ IDEA.</h3><br>
+ * <b>Project:</b> Programming-Exercise - Map Collections<br>
+ * <b>User:</b> Simon Schoeggler<br>
+ * <b>Date:</b> 12. April 2023<br>
+ * <b>Time:</b> 08:55<br>
+ */
+
+public class DayPlanner {
+    private Map<LocalDate, ArrayList<Entry>> map = new LinkedHashMap<>();
+
+    public static void main(String[] args) {
+        DayPlanner dayPlanner = new DayPlanner();
+
+        dayPlanner.addEntry(LocalDate.of(2019, 10, 19), new Entry(LocalTime.of(17, 0), "Party"));
+        dayPlanner.addEntry(LocalDate.of(2019, 10, 19), new Entry(LocalTime.of(10, 30), "Fussball"));
+        dayPlanner.addEntry(LocalDate.of(2019, 10, 18), new Entry(LocalTime.of(17, 0), "POS Aufgabe abgeben"));
+        dayPlanner.addEntry(LocalDate.of(2019, 10, 18), new Entry(LocalTime.of(8, 0), "Pair Programming POS"));
+        dayPlanner.addEntry(LocalDate.of(2019, 10, 18), new Entry(LocalTime.of(6, 30), "Frühstück"));
+
+        File file = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "entries.csv").toFile();
+        dayPlanner.loadEntries(file);
+        dayPlanner.showAllDays();
+        dayPlanner.showEntriesOfDay(LocalDate.of(2019, 10, 18));
+
+        // grouped by years
+        /*
+        dayPlanner.map.keySet().stream().collect(Collectors.groupingBy(LocalDate::getYear)).forEach((year, dates) -> {
+            System.out.println(year);
+            dates.forEach(date -> {
+                dayPlanner.map.get(date).forEach(System.out::println);
+            });
+        });
+        */
+
+    }
+
+    private void loadEntries(File file) {
+        try (Stream<String[]> stream = Files.lines(file.toPath()).skip(1).map(line -> line.split(","))) {
+            stream.forEach(split -> {
+                LocalDate date = LocalDate.parse(split[0]);
+                LocalTime time = LocalTime.parse(split[2]);
+                String text = split[1];
+                addEntry(date, new Entry(time, text));
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addEntry(LocalDate date, Entry entry) {
+        /*map.computeIfPresent(date, (key, value) -> {
+            value.add(entry);
+            return value;
+        });
+
+        map.computeIfAbsent(date, key -> {
+            ArrayList<Entry> entries = new ArrayList<>();
+            entries.add(entry);
+            return entries;
+        });*/
+        map.merge(date, new ArrayList<>(List.of(entry)), (oldList, newList) -> {
+            if (oldList
+                    .stream()
+                    .map(Entry::getTime)
+                    .anyMatch(time -> newList
+                            .stream()
+                            .map(Entry::getTime)
+                            .anyMatch(time::equals))) {
+                System.out.println("Entry-Time already exists!");
+            } else {
+                oldList.addAll(newList);
+                oldList.sort(Comparator.comparing(Entry::getTime));
+            }
+            return oldList;
+        });
+    }
+
+    private void showEntriesOfDay(LocalDate date) {
+        if (map.containsKey(date)) {
+            System.out.println("<" + date + ">");
+            map.get(date).forEach(System.out::println);
+        } else {
+            System.out.println("No entries for this day!");
+        }
+    }
+
+    private void showAllDays() {
+        System.out.println("Datum\t\tEinträge");
+        System.out.println("------------------------------");
+        map.forEach((date, entries) -> {
+            System.out.println("<" + date + ">");
+            entries.forEach(System.out::println);
+        });
+    }
+}
---
+++ src/main/java/at/kaindorf/stream/Entry.java
@@ -0,0 +1,31 @@
+package at.kaindorf;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * <h3>Created by IntelliJ IDEA.</h3><br>
+ * <b>Project:</b> Programming-Exercise - Map Collections<br>
+ * <b>User:</b> Simon Schoeggler<br>
+ * <b>Date:</b> 12. April 2023<br>
+ * <b>Time:</b> 08:53<br>
+ */
+
+@Data
+@AllArgsConstructor
+public class Entry {
+
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+
+    private LocalTime time;
+    private String text;
+
+    @Override
+    public String toString() {
+        return "\t\t" + time.format(dtf) + " " + text;
+    }
+}
